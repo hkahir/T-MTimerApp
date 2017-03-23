a0d15c043bcdba1218e7b9186e11b3a805ca30e7
hkahir <hardik.silversky@gmail.com>
2017-03-23 13:39:47 +0530

    add comment line

---
+++ app/src/main/java/com/t_mtimer/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.t_mtimer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeActivity extends Activity {
+
+    private Button btnStart;
+    private Activity activity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        activity = HomeActivity.this;
+
+        btnStart = (Button) findViewById(R.id.btn_start);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(activity, MeditationActivity.class);
+                startActivity(i);
+
+
+//                Call function
+
+            }
+        });
+
+    }
+}
